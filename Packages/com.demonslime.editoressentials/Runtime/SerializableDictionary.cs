1c12ed00e271004f63e76265e860c06ed69fa788
Sujyot <70472979+sujyotraut@users.noreply.github.com>
2024-11-01 19:38:39 +0530

    Project setup

---
+++ Packages/com.demonslime.editoressentials/Runtime/SerializableDictionary.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+
+namespace EditorEssentials {
+    [Serializable]
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue> { }
+}
